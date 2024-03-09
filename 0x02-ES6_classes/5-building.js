cc313ebd82b68693adc3ce40d904f92bb6b3c626
injili <nyarekigospel@gmail.com>
2024-03-09 20:33:29 +0000

    just for tonight

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft){
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+}
