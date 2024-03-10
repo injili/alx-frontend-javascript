e824c840a5e3ba168772f06590716fd049917250
injili <nyarekigospel@gmail.com>
2024-03-10 14:51:47 +0000

    this is back

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,13 @@
 export default class Building {
-  constructor(sqft){
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
+  constructor(sqfti = 0){
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage != 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(newSqft) {
-    if (typeof newSqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
-    this._sqft = newSqft;
   }
 }
