cf5760ead06e997748d00a7b022056c689749465
injili <nyarekigospel@gmail.com>
2024-03-10 15:33:36 +0000

    building is rectified

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
-  constructor(sqfti = 0){
+  constructor(sqft = 0) {
     if (this.constructor !== Building
-        && typeof this.evacuationWarningMessage != 'function') {
+        && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
