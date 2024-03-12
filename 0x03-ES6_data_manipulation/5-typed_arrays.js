c775927382559b11cd20904aedc1a0f8bcb167d0
injili <nyarekigospel@gmail.com>
2024-03-12 18:21:22 +0000

    typed array is in play

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (length - 1 <= position || position < 0) {
-    throw new Error('Position outside range');
-  }
   const arrayB = new ArrayBuffer(length);
-  arrayB[position] = value;
-  return (arrayB);
+  const arrayD = new DataView(arrayB);
+  try {
+    arrayD.setInt8(position, value);
+  } catch (e) {
+    throw Error('Position outside range');
+  }
+  return (arrayD);
 }
