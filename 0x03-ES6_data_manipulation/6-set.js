b95ea4237f555356eb9fe91f063390c1664f8b53
injili <nyarekigospel@gmail.com>
2024-03-13 00:01:05 +0300

    we are running locally

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,2 +1,7 @@
-export default function setFromArray() {
-  
+export default function setFromArray(array) {
+  const mySet = new Set();
+  for (const a of array) {
+    mySet.add(a);
+  }
+  return mySet;
+}
