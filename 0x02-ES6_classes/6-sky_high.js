a7f769c2fe2a42ab5e54878c09d3a6ac325326b4
injili <nyarekigospel@gmail.com>
2024-03-15 14:37:03 +0300

    the inheritance is in play my peeps

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,27 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (!(typeof(sqft) === 'number')) {
+        return ('sqft must be a number')
+    }
+    if (!(typeof(floors) === 'number')) {
+        return ('floors must be a number')
+    }
+
+    super(sqft);
+    this._floor = floors;
+  }
+
+  get floors() {
+    return this._floor;
+  }
+
+  get sqft() {
+    return super.sqft;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floor} floors`;
+  }
+}
